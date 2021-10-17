74adec91d845e312bd0705eb9db75c94bd2617a8
joshmclynn <mclynn_josh@hotmail.co.uk>
2021-10-17 17:45:56 +0100

    restart

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -1,22 +1,58 @@
 
-var canvas = document.getElementById("gameScreen");
-var heightRatio=1;
-canvas.height = canvas.width * heightRatio;
+
+var canvas = document.getElementById("gameScreen");   // the canvas where game will be drawn
+var context = canvas.getContext("2d");            // canvas context
+var levelCols=11;							// level width, in tiles
+var levelRows=9;							// level height, in tiles
+var tileSize=32;							// tile size, in pixels
+var playerCol=5;                                  // player starting column
+var playerRow=4;                                  // player starting row
+
+var level = [        						// the 11x9 level - 1=wall, 0=empty space
+    [1,1,1,1,1,1,1,1,1,1,1],
+    [1,1,0,0,0,0,0,0,0,1,1],
+    [1,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,1,0,1,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,1,0,1,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,1],
+    [1,1,0,0,0,0,0,0,0,1,1],
+    [1,1,1,1,1,1,1,1,1,1,1]
+];
+
+var playerYPos=playerRow*tileSize;				// converting Y player position from tiles to pixels
+var playerXPos=playerCol*=tileSize;               // converting X player position from tiles to pixels
+
+canvas.width=tileSize*levelCols;                   // canvas width. Won't work without it even if you style it from CSS
+canvas.height=tileSize*levelRows;                   // canvas height. Same as before
+
+renderLevel();
+
+// function to display the level
+var mow1=new Image();
+mow1=onload=start;
+mow1.src="assets/img/mower2d.png";
+function renderLevel(){
+    // clear the canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    // walls = red boxes
+    context.fillStyle = "#ff0000";
+    for(i=0;i<levelRows;i++){
+        for(j=0;j<levelCols;j++){
+            if(level[i][j]==1){
+                context.fillRect(j*tileSize,i*tileSize,tileSize,tileSize);	
+            }
+        }
+    }
+    //player = green box
+    context.fillStyle = mow1;
+    context.fillRect(playerXPos,playerYPos,tileSize,tileSize);
+}
 
 
-var context = canvas.getContext("2d");
-//Objects
-var mower = document.getElementById("mower2d");
-var can = document.getElementById("fuelCan");
-//Initial Positions
-var mowx = 0.0;
-var mowy = 0.0;
 
 
 
-//variables
-var fuelLevel = 10;//initial
-var maxFuel = 30; //max
 
 
 
