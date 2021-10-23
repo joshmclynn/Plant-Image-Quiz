56e5748256aa8876a6ca3582ad209070eefef227
joshmclynn <mclynn_josh@hotmail.co.uk>
2021-10-23 16:41:49 +0100

    01

---
+++ assets/js/scipt.js
@@ -0,0 +1,19 @@
+
+
+const question01 = 
+{ 
+  question: "assets\img\anenome.jpg",
+    answers: {
+      a : "Anenome",
+      b : "Osmanthus",
+      c : "Rose",
+      d : "SunFlower"
+    },
+    correct:"a"
+};
+
+function newQuestion(){
+  
+}
+
+
