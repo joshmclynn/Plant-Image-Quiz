5c0e5595773296345e85e66685fd4b4b4b1f3a2c
joshmclynn <mclynn_josh@hotmail.co.uk>
2021-10-26 20:32:35 +0100

    1

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -1,19 +1,27 @@
+const fiftyFity = document.getElementById("fiftyfifty");
+const audience = document.getElementById("audience");
+const friend = document.getElementById("friend")
+const image = document.getElementById("imageArea");
+ctx = image.getContext('2d');
 
 
-const question01 = 
-{ 
-  question: "assets\img\anenome.jpg",
-    answers: {
-      a : "Anenome",
-      b : "Osmanthus",
-      c : "Rose",
-      d : "SunFlower"
-    },
-    correct:"a"
-};
-
-function newQuestion(){
-  
+//images
+var images = {
+  "anenomePic" :"assets\img\anenome.jpg",
+  "erigeronpic" :"assets\img\erigeron.jpg",
+  "osmanthuspic" :"assets\img\osmanthus.jpg"
 }
+
+//Questions
+
+new newQuestion("anenomePic",["Anenome","Rose","Tree","Sunflower"],"anenome");
+
+function question(image,choices,answer){
+  this.image = image;
+  this.choice = choices;
+  this.answer = answer;
+
+}
+  
 
 
