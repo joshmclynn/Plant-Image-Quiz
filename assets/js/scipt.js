3d66f5d5aae3f8adec20c30b9a958a8e4e275d8b
joshmclynn <mclynn_josh@hotmail.co.uk>
2021-11-04 17:44:14 +0000

    adding js for loading questions randomly

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -1,24 +1,39 @@
 const start = document.getElementById('startQuiz');
 const questioncon = document.getElementById('imageQuestion');
 const quiz = document.getElementById("game");
-const answerbutton = document.getElementById('answerbtn');
+
 start.addEventListener('click',newGame);
 
-let  currentQuestion;
+let  currentQuestion,randomNumberArr;
 
 
 function newGame(){
-    getNextQuestion();
-    randomQuestion = questionArr.sort();
-    currentQuestion = 0;
-
+    
+    
+    currentQuestion = 0
+    randomNumberArr = questionArr.sort(()=> Math.random() - .5)
+    getNextQuestion()
 }
+
+
+
+
 function getNextQuestion(){
-    loadQuestion(questionArr[1]);
+   
+    loadQuestion(randomNumberArr[currentQuestion]);
+    document.getElementById("answer1").innerHTML = questionArr[currentQuestion].choices[0];
+    document.getElementById("answer2").innerHTML = questionArr[currentQuestion].choices[1];
+    document.getElementById("answer3").innerHTML = questionArr[currentQuestion].choices[2];
+    document.getElementById("answer4").innerHTML = questionArr[currentQuestion].choices[3];
 }
 function loadQuestion(question){
     questioncon.innerHTML = question.question;
+    
+      
+    
+    
 }
+
 
 
 /**Question list
